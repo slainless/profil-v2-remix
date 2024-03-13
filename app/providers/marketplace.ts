b5c888c4d7f0397d407edf756162c3b281de62c0
Aiman Fauzy <aimanfauzy.99@gmail.com>
2024-03-13 15:02:24 +0800

    fix: should dangerously hydrate on client

--- app/providers/marketplace.ts
+++ app/providers/marketplace.ts
@@ -23,7 +23,7 @@ export const MarketItemHydrator = ({
       [selectedImageAtom, initialSelectedImage],
     ],
     {
-      // dangerouslyForceHydrate: true,
+      dangerouslyForceHydrate: true,
     },
   )
 
